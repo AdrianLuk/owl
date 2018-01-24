4c89e2d719275986d7be3128e122855886fcf78c
Adrian Luk <adrian.luk@senecacollege.ca>
2018-01-24 16:19:01 -0500

    adding routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
-import { fetchTeams } from './components/api-handler';
+// import { fetchTeams } from './components/api-handler';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./components/home/home";
+import Players from './components/players/players';
+import Schedule from './components/schedule/schedule';
+import Teams from './components/teams/teams';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-
-      </div>
+      <Router>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/teams' component={Teams} />
+          <Route path='/players' component={Players} />
+          <Route path='/schedule' component={Schedule} />
+        </Switch>
+      </Router>
     );
   }
 }
---
+++ src/components/schedule/schedule.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Schedule extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Schedule page</h1>
+            </div>
+        );
+    }
+}
+
+export default Schedule;
