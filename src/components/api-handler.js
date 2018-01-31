8ee71711e1ccd8f33f48c378facef91a10524ce1
Adrian Luk <adrian.luk@senecacollege.ca>
2018-01-31 16:12:09 -0500

    added a semicolon lul

--- src/components/api-handler.js
+++ src/components/api-handler.js
@@ -6,5 +6,5 @@ export function fetchTeams(team_id) {
     return new Promise((resolve, reject) => {
         let teams_url = `${apiUrl}/teams/${team_id}`;
         axios.get(teams_url).then(data => resolve(data)).catch(err => reject(err));
-    })
+    });
 }
