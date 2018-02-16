23ff6491fbd4f36ad1127aead79f6ac411bc291c
Adrian <lukabaseball@hotmail.com>
2018-02-16 01:55:17 -0500

    added reactstrap and took out react-bootstrap

--- src/components/common/headers/main-header.js
+++ src/components/common/headers/main-header.js
@@ -1,41 +1,64 @@
 import React, { Component } from 'react';
-import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
+
 // import { Link } from 'react-router-dom';
+
+import {
+    Container,
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem
+} from 'reactstrap';
 import './main-header.css';
+
 export default class MainHeader extends Component {
+    constructor(props) {
+        super(props);
+
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            isOpen: false
+        };
+    }
+    toggle() {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
     render() {
         return (
-            <div>
-                <Navbar inverse collapseOnSelect>
-                    <Navbar.Header>
-                        <Navbar.Brand>
-                            <a href="#brand">React-Bootstrap</a>
-                        </Navbar.Brand>
-                        <Navbar.Toggle />
-                    </Navbar.Header>
-                    <Navbar.Collapse>
-                        <Nav>
-                            <NavItem eventKey={1} href="#">Link</NavItem>
-                            <NavItem eventKey={2} href="#">Link</NavItem>
-                            <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-                                <MenuItem eventKey={3.1}>Action</MenuItem>
-                                <MenuItem eventKey={3.2}>Another action</MenuItem>
-                                <MenuItem eventKey={3.3}>Something else here</MenuItem>
-                                <MenuItem divider />
-                                <MenuItem eventKey={3.3}>Separated link</MenuItem>
-                            </NavDropdown>
+            <Container>
+                <Navbar color="dark" dark expand="lg">
+                    <NavbarBrand href="/">Overwatch League</NavbarBrand>
+                    <NavbarToggler onClick={this.toggle} />
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        <Nav className="ml-auto" navbar>
+                            <NavItem>
+                                <NavLink href="#">Components</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="#">Github</NavLink>
+                            </NavItem>
+                            <UncontrolledDropdown nav>
+                                <DropdownToggle nav caret>Options</DropdownToggle>
+                                <DropdownMenu >
+                                    <DropdownItem>Option 1</DropdownItem>
+                                    <DropdownItem>Option 2</DropdownItem>
+                                    <DropdownItem divider />
+                                    <DropdownItem>Reset</DropdownItem>
+                                </DropdownMenu>
+                            </UncontrolledDropdown>
                         </Nav>
-                        <Nav pullRight>
-                            <NavItem eventKey={1} href="#">
-                                Link Right
-      </NavItem>
-                            <NavItem eventKey={2} href="#">
-                                Link Right
-      </NavItem>
-                        </Nav>
-                    </Navbar.Collapse>
+                    </Collapse>
                 </Navbar>
-            </div>
+            </Container>
         );
     }
 }
