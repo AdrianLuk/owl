40d305f387962a344ecfbb2a1f4a394ec9246143
Adrian <lukabaseball@hotmail.com>
2018-02-22 03:05:33 -0500

    added redux dependencies

--- src/index.js
+++ src/index.js
@@ -2,8 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from 'redux';
 import App from './app/App';
+import reducers from './reducers';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+cont createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+    <Provider store={createStoreWithMiddleware(reducers)}>
+        <App />
+    </Provider>
+    , document.getElementById('root'));
 registerServiceWorker();
