d27b9f59329766094c49ae28df8d1d130503012c
Adrian <lukabaseball@hotmail.com>
2018-02-22 22:30:47 -0500

    styling and trying to get collapse to work

--- src/components/teams/teams.js
+++ src/components/teams/teams.js
@@ -3,18 +3,47 @@ import React, { Component } from 'react';
 import { connect } from "react-redux";
 import { fetchTeams } from '../../actions';
 import MainHeader from '../common/headers/main-header';
-import { Container, Card, CardImg, CardTitle, CardText, CardGroup, CardBody, CardLink } from 'reactstrap';
+import {
+    Collapse,
+    Container,
+    Card,
+    CardImg,
+    CardTitle,
+    CardText,
+    CardGroup,
+    CardBody,
+    CardLink
+} from 'reactstrap';
+// import { LinkContainer } from 'react-router-bootstrap';
 
 import './teams.css';
 
 class Teams extends Component {
+    constructor(props) {
+        super(props);
+        this.toggle = this
+            .toggle
+            .bind(this);
+        this.state = {
+            collapse: false
+        };
+    }
+
     componentDidMount() {
-        this.props.fetchTeams();
+        this
+            .props
+            .fetchTeams();
+    }
+
+    toggle() {
+        this.setState({
+            collapse: !this.state.collapse
+        });
     }
 
     renderTeams() {
         return _.map(this.props.teams, team => {
-            console.log(team)
+            // console.log(team)
             return (
                 <div className="CardGroup-card" key={team.competitor.id}>
                     <Card className="card-team">
@@ -23,7 +52,13 @@ class Teams extends Component {
                             <CardTitle className="font-weight-bold">{team.competitor.name}</CardTitle>
                             <CardText>{team.competitor.homeLocation}</CardText>
                         </CardBody>
+                        <CardLink
+                            onClick={this.toggle}
+                            className="card-button card-link--withUnderlineAnimation">View Roster</CardLink>
                     </Card>
+                    <Collapse isOpen={this.state.collapse}>
+                        <div>{team.competitor.name} {team.competitor.players[0].player.name}</div>
+                    </Collapse>
                 </div>
             );
         });
@@ -32,15 +67,10 @@ class Teams extends Component {
     render() {
         return (
             <div>
-                <MainHeader />
-                <h1>Teams</h1>
-                <Container>
-                    <CardGroup>
-                        {this.renderTeams()}
-                    </CardGroup>
-                </Container>
-            </div>
-        );
+                <MainHeader /> <h1>Teams</h1> <Container> <CardGroup>
+                    {this.renderTeams()}
+                </CardGroup> </Container>
+            </div >);
     }
 }
 
