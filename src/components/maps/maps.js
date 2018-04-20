78b30ee91d3eb54c0f2da191e407decdbdffba0f
AdrianLuk <lukabaseball@hotmail.com>
2018-04-19 20:52:47 -0400

    getting my data onto the screen

--- src/components/maps/maps.js
+++ src/components/maps/maps.js
@@ -1,15 +1,49 @@
 import React, { Component } from "react";
 import MainHeader from "../common/headers/main-header";
+import { fetchMaps } from '../../actions';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import './maps.css';
 
 class Maps extends Component {
+    constructor(props) {
+        super(props);
+        this.renderMapList = this.renderMapList.bind(this);
+    }
+
+    componentDidMount() {
+        this.props.fetchMaps();
+    }
+
+    renderMapList() {
+        // console.log(this.props.maps);
+        const maps = this.props.maps.map(map => {
+            return (
+                <a href="javascript:void(0)">{map.name}</a>
+            );
+        });
+    }
     render() {
         return (
             <div>
                 <MainHeader />
                 <h1>Maps Page</h1>
+                {this.renderMapList()}
             </div>
         );
     }
 }
 
-export default Maps;
+function mapStateToProps(state) {
+    return {
+        maps: state.maps
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators(
+        { fetchMaps: fetchMaps }, dispatch
+    );
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Maps);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import TeamsReducer from "./teams-reducer";
 import ActiveTeam from "./active-team-reducer";
 import ScheduleReducer from './schedule-reducer';
+import MapsReducer from './maps-reducer';
 
 const rootReducer = combineReducers({
     teams: TeamsReducer,
     activeTeam: ActiveTeam,
-    schedule: ScheduleReducer
+    schedule: ScheduleReducer,
+    maps: MapsReducer
 });
 
 export default rootReducer;
